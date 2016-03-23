91af63cac9968caf0a9418ed3905261946b9a47e
Jacob Blakely <jacob@codekirei.com>
2016-03-22 17:19:27 -0700

    back to browser-sync; was barking up wrong tree

--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -1,54 +1,28 @@
 #! node_modules/.bin/babel-node
 
-// modules
-//---------------------------------------------------------
 import path from 'path'
-import express from 'express'
-import firstOpenPort from 'first-open-port'
 import webpack from 'webpack'
 import makeConfig from './webpack.config'
+import browserSync from 'browser-sync'
 import devMiddleware from 'webpack-dev-middleware'
 import hotMiddleware from 'webpack-hot-middleware'
 
-// setup
-//---------------------------------------------------------
-const cwd = process.cwd()
 const config = makeConfig()
-const server = express()
 const bundler = webpack(config)
+const server = browserSync.create('dev')
+const baseDir = 'src'
 
-// utilities
-//---------------------------------------------------------
-const logAndExit = err => {
-  console.error(err.stack)
-  process.exit(1)
-}
-
-// middleware
-//---------------------------------------------------------
-server.use(devMiddleware(bundler, {
-  noInfo: true,
-  publicPath: config.output.publicPath,
-}))
-
-server.use(hotMiddleware(bundler))
-
-// routes
-//---------------------------------------------------------
-server.get('/', (req, res) => {
-  res.sendFile(path.join(cwd, 'src', 'index.html'))
+server.init({
+  open: false,
+  files: [path.join(baseDir, 'index.html')],
+  server: {
+    baseDir,
+    middleware: [
+      devMiddleware(bundler, {
+        publicPath: config.output.publicPath,
+        noInfo: true,
+      }),
+      hotMiddleware(bundler),
+    ],
+  },
 })
-
-// launch
-//---------------------------------------------------------
-firstOpenPort(3000, 3100)
-  .then(port => {
-    server
-      .listen(port, () => {
-        console.info(`dev server listening at localhost:${port}`)
-      })
-      .on('error', logAndExit)
-  })
-  .catch(logAndExit)
-
-/* eslint no-console: 0 */
