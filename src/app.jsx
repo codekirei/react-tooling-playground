122ec29ff50697c2fec7b8df0430d1f0e034b8e2
Jacob Blakely <jacob@codekirei.com>
2016-03-22 17:06:44 -0700

    fix extra hot reload call that was causing bugs

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import Root from './components/root.jsx'
 import './styles/index.css'
 
 const rootEl = document.getElementById('root')
-const render = () => ReactDOM.render(<Root />, rootEl)
-
-if (module.hot) module.hot.accept()
-render()
+render(<Root />, rootEl)
